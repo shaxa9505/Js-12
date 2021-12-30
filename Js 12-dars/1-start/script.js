4880c84e4affbf6b113564c9a8818bf197181229
shaxa9505 <87860218+shaxa9505@users.noreply.github.com>
2021-12-30 14:59:01 +0500

    js12
    js12

---
+++ Js 12-dars/1-start/script.js
@@ -0,0 +1,37 @@
+// var div = document.querySelector(".text");
+
+
+// div.onclick = function () {
+//     div.style.background = "green";
+// }
+
+
+
+
+// i kiyn js headdi icida iwlawi ucun window.onloaddi kursatasan 
+
+
+
+// alert("3") bulani suresan qaysi biri 1-ciqiwini
+
+// window.addEventListener("DOMContentLoaded", () => {})
+window.onload = function () {
+    var div = document.querySelector(".text");
+
+
+    // div.onclick = function () {
+    //     div.style.background = "green";
+    // }
+    
+    div.onclick = setbg // i kiyn manerda funcktsiya nmeya caqirmaganimzni tuwuntirasan
+    function setbg () {
+        div.style.color = "red"
+    }
+
+    // alert("2")
+
+
+
+}
+
+// alert("3")
